46327ac1c7db6918073ea7267d3e4e6e4d172830
Olga-Olga <olga_l@ukr.net>
2023-08-12 20:19:23 +0300

    extra

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, NavLink, Outlet } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import { Layout } from './Layout.jsx';
 import { Test } from './Test.jsx';
 
