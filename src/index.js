51947053f99b0a33372db7c59310b4a076bd3dd1
Olga-Olga <olga_l@ukr.net>
2023-08-14 15:48:46 +0300

    git page

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter>
-    {/* <BrowserRouter basename="goit-react-hw-05-movies"> */}
+  // <BrowserRouter>
+  <BrowserRouter basename="goit-react-hw-05-movies">
     <App />
     {/* <GlobalStyles /> */}
     {/* <ToastContainer autoClose={1000} /> */}
